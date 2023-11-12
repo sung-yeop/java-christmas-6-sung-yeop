3c2fc85b9e53befbbf4ec29b5badc4e1452f5fcd
sung-yeop <jjabc9459@naver.com>
2023-11-12 15:14:53 +0900

    feat(Validate, ValidateTest): 메뉴 존재 여부 유효성 검사 기능 추가
    - 사용자가 주문한 메뉴가 존재하는지 확인하는 유효성 검사 기능 추가

--- src/main/java/christmas/validate/Validate.java
+++ src/main/java/christmas/validate/Validate.java
@@ -1,14 +1,35 @@
 package christmas.validate;
 
 import christmas.constants.Constants;
+import christmas.domain.Appetizer;
+import christmas.domain.DesertMenu;
+import christmas.domain.DrinkMenu;
+import christmas.domain.MainMenu;
 
 public class Validate {
-    public static final String errorMessage ="[ERROR]";
+    public static final String errorMessage = "[ERROR]";
+
+    public static void menuValidate(String menuName) {
+        try {
+            checkMenu(menuName);
+            ;
+        } catch (IllegalArgumentException e) {
+            System.out.println(errorMessage);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkMenu(String menuName) {
+        if (Appetizer.getPriceWithName(menuName) == 0 && DesertMenu.getPriceWithName(menuName) == 0
+                && DrinkMenu.getPriceWithName(menuName) == 0 && MainMenu.getPriceWithName(menuName) == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static void visitDateValidate(int visitDate) {
-        try{
+        try {
             checkVisitDate(visitDate);
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println(errorMessage);
             throw new IllegalArgumentException();
         }
--- src/test/java/christmas/validate/ValidateTest.java
+++ src/test/java/christmas/validate/ValidateTest.java
@@ -1,23 +1,34 @@
 package christmas.validate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidateTest {
     Validate validate;
 
-    private final static String errorMessage = "[ERROR]";
-
     @BeforeEach
     void Before() {
         validate = new Validate();
+    }
+
+    @DisplayName("메뉴판에 주문한 메뉴가 존재하지 않는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"지로콜라", "케이크", "치킨", "피자"})
+    void 메뉴_존재_X(String menuName) {
+        assertThatThrownBy(() -> validate.menuValidate(menuName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴판에 주문한 메뉴가 존재하는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"제로콜라", "초코케이크", "티본스테이크", "타파스"})
+    void 메뉴_존재_O(String menuName) {
+        assertThatNoException().isThrownBy(() -> validate.menuValidate(menuName));
     }
 
     @DisplayName("1 ~ 31 외의 입력은 모두 에러 처리")
@@ -26,14 +37,14 @@ class ValidateTest {
     void 날짜_유효성_검사_오류O(int visitDate) {
 
         //given
-        org.assertj.core.api.Assertions.assertThatThrownBy(() -> validate.visitDateValidate(visitDate))
+        assertThatThrownBy(() -> validate.visitDateValidate(visitDate))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("1 ~ 31 사이의 입력은 모두 정상 기능")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 6, 31})
-    void 날짜_유효성_검사_오류X(int visitDate){
-        org.assertj.core.api.Assertions.assertThatNoException().isThrownBy(() -> validate.visitDateValidate(visitDate));
+    void 날짜_유효성_검사_오류X(int visitDate) {
+        assertThatNoException().isThrownBy(() -> validate.visitDateValidate(visitDate));
     }
 }
