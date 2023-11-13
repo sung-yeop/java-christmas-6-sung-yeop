538ee91536489f1fadbfd6957f0c599c0657ad61
sung-yeop <jjabc9459@naver.com>
2023-11-13 17:33:11 +0900

    feat(Input): 날짜 입력 기능 및 메뉴 입력 기능 구현
    - 사용자로부터 날짜 입력 기능 및 메뉴 입력 기능 구현
    - 해당 기능에서 유효성 검사 진행

--- src/main/java/christmas/model/Input.java
+++ src/main/java/christmas/model/Input.java
@@ -1,2 +1,51 @@
-package christmas.model;public class Input {
+package christmas.model;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.validate.Validate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Input {
+
+    private static final String delimeterComma = "(,)";
+    private static final String delimeterDash = "(-)";
+
+    public int inputOrderDate() {
+        String input = Console.readLine();
+        try {
+            Validate.integerTypeValidate(input);
+            int date = Integer.parseInt(input);
+            Validate.visitDateValidate(date);
+        } catch (IllegalArgumentException e) {
+            inputOrderDate();
+        }
+        return Integer.parseInt(input);
+    }
+
+    public Map<String, Integer> inputOrderMenu() {
+        String input = Console.readLine();
+        try {
+            Validate.menuFormatValidate(input);
+            Validate.menuDuplicationValidate(input);
+            Validate.menuValidate(input);
+            Validate.menuOrderNumberValidate(input);
+        } catch (IllegalArgumentException e) {
+            inputOrderMenu();
+        }
+        return createOrder(input);
+    }
+
+    private Map<String, Integer> createOrder(String input) {
+        Map<String, Integer> result = new HashMap<>();
+        List<String[]> pair = Arrays.stream(input.split(delimeterComma))
+                .map(s -> s.split(delimeterDash)).collect(Collectors.toList());
+        for (String[] strings : pair) {
+            result.put(strings[0], Integer.parseInt(strings[1]));
+        }
+        return result;
+    }
 }
