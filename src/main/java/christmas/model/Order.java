d4e9301dd4f1c20157cdae38c350f3c82e2d79a9
sung-yeop <jjabc9459@naver.com>
2023-11-13 18:33:23 +0900

    feat(Order, OrderTest): 할인 적용 이전 구매 총량 반환 기능 추가
    - 할인 적용 이전에 구매한 금액의 총액을 반환하는 기능 추가
    - 테스트 코드 작성

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,22 +1,20 @@
 package christmas.model;
 
-import java.util.HashMap;
+import christmas.domain.Appetizer;
+import christmas.domain.DesertMenu;
+import christmas.domain.DrinkMenu;
+import christmas.domain.MainMenu;
+
 import java.util.Map;
 
 public class Order {
 
-    private int orderAmount;
     private int orderDate;
-    private Map<String, Integer> orderMenu = new HashMap<>();
+    private Map<String, Integer> orderMenu;
 
-    public Order(int orderAmount, int orderDate, Map<String, Integer> orderMenu) {
-        this.orderAmount = orderAmount;
+    public Order(int orderDate, Map<String, Integer> orderMenu) {
         this.orderDate = orderDate;
         this.orderMenu = orderMenu;
-    }
-
-    public int getOrderAmount() {
-        return orderAmount;
     }
 
     public int getOrderDate() {
@@ -25,5 +23,22 @@ public class Order {
 
     public Map<String, Integer> getOrderMenu() {
         return orderMenu;
+    }
+
+    public int accountAmount() {
+        int amount = 0;
+        for (String input : orderMenu.keySet()) {
+            amount += menuPrice(input) * orderMenu.get(input);
+        }
+        return amount;
+    }
+
+    private int menuPrice(String key) {
+        int amount = 0;
+        amount += Appetizer.getPriceWithName(key);
+        amount += DesertMenu.getPriceWithName(key);
+        amount += DrinkMenu.getPriceWithName(key);
+        amount += MainMenu.getPriceWithName(key);
+        return amount;
     }
 }
---
+++ src/test/java/christmas/model/OrderTest.java
@@ -0,0 +1,33 @@
+package christmas.model;
+
+import christmas.domain.DesertMenu;
+import christmas.domain.DrinkMenu;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class OrderTest {
+
+    private Order order;
+    private static final int weekend = 1;
+    private static final int holiday = 25;
+
+    @BeforeEach
+    void setOrder() {
+        Map<String, Integer> orderMenu = new HashMap<>();
+        orderMenu.put("초코케이크", 3);
+        orderMenu.put("샴페인", 5);
+        order = new Order(weekend, orderMenu);
+    }
+
+    @Test
+    void 할인_전_구입_금액_반환() {
+        Assertions.assertThat(order.accountAmount())
+                .isEqualTo(DesertMenu.CAKE.getPrice() * 3 + DrinkMenu.CHAMPAGNE.getPrice() * 5);
+    }
+
+
+}
