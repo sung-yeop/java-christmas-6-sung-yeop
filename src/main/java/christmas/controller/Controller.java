63ade21ceb742d595c221da07bfdb33145d9c4a5
sung-yeop <jjabc9459@naver.com>
2023-11-15 19:23:54 +0900

    style(Controller): intend 수정
    - 잘못 적용된 intend 수정

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -14,63 +14,63 @@ import java.util.Map;
 
 public class Controller {
 
-  private BadgeEvent badgeEvent;
-  private Condition condition;
-  private Discount discount;
-  private Input input;
-  private OutputView outputView;
-  private InputView inputView;
-  private Order order;
+    private BadgeEvent badgeEvent;
+    private Condition condition;
+    private Discount discount;
+    private Input input;
+    private OutputView outputView;
+    private InputView inputView;
+    private Order order;
 
-  public Controller(BadgeEvent badgeEvent, Condition condition, Discount discount, Input input
-          , OutputView outputView, InputView inputView) {
-    this.badgeEvent = badgeEvent;
-    this.condition = condition;
-    this.discount = discount;
-    this.input = input;
-    this.outputView = outputView;
-    this.inputView = inputView;
-  }
-
-  public void start() {
-    setOrder();
-
-    outputView.viewPreviewMessage();
-    Map<String, Integer> discountTotal = new LinkedHashMap<>();
-
-    if (checkCondition()) {
-      discountTotal = discount.discountTotal(order.getOrderDate(),
-              order.getOrderMenu(), order.orderAmount());
-      outputEvent(true, discountTotal);
+    public Controller(BadgeEvent badgeEvent, Condition condition, Discount discount, Input input
+            , OutputView outputView, InputView inputView) {
+        this.badgeEvent = badgeEvent;
+        this.condition = condition;
+        this.discount = discount;
+        this.input = input;
+        this.outputView = outputView;
+        this.inputView = inputView;
     }
-    if (!checkCondition()) {
-      outputEvent(false, discountTotal);
+
+    public void start() {
+        setOrder();
+
+        outputView.viewPreviewMessage();
+        Map<String, Integer> discountTotal = new LinkedHashMap<>();
+
+        if (checkCondition()) {
+            discountTotal = discount.discountTotal(order.getOrderDate(),
+                    order.getOrderMenu(), order.orderAmount());
+            outputEvent(true, discountTotal);
+        }
+        if (!checkCondition()) {
+            outputEvent(false, discountTotal);
+        }
     }
-  }
 
-  private void outputEvent(boolean benefit, Map<String, Integer> discountTotal) {
-    outputView.viewMenuOutput(order.getOrderMenu());
-    outputView.viewOrderAmountOutput(order.orderAmount());
-    outputView.viewBonusMenuOutput(discount.eventChampagne(order.orderAmount()));
-    outputView.viewBenefitOutput(discountTotal);
-    outputView.viewAllBenefitAmount(benefit, discount.discountTotalAmount(discountTotal));
-    outputView.viewAmountOfPayment(order.orderAmount() - discount.discountTotalAmountExceptBonus(discountTotal));
-    outputView.viewBadge(badgeEvent.giveBadge(order.orderAmount()));
-  }
+    private void outputEvent(boolean benefit, Map<String, Integer> discountTotal) {
+        outputView.viewMenuOutput(order.getOrderMenu());
+        outputView.viewOrderAmountOutput(order.orderAmount());
+        outputView.viewBonusMenuOutput(discount.eventChampagne(order.orderAmount()));
+        outputView.viewBenefitOutput(discountTotal);
+        outputView.viewAllBenefitAmount(benefit, discount.discountTotalAmount(discountTotal));
+        outputView.viewAmountOfPayment(order.orderAmount() - discount.discountTotalAmountExceptBonus(discountTotal));
+        outputView.viewBadge(badgeEvent.giveBadge(order.orderAmount()));
+    }
 
-  private boolean checkCondition() {
-    return condition.checkEventConditionAmount(order.orderAmount()) && condition.checkEventConditionMenu(order.getOrderMenu())
-            && condition.checkEventConditionMenuCount(order.getOrderMenu());
-  }
+    private boolean checkCondition() {
+        return condition.checkEventConditionAmount(order.orderAmount()) && condition.checkEventConditionMenu(order.getOrderMenu())
+                && condition.checkEventConditionMenuCount(order.getOrderMenu());
+    }
 
-  private void setOrder() {
-    inputView.viewMainMessage();
-    inputView.viewInputDateMessage();
-    int date = input.inputOrderDate();
-    inputView.viewInputMenuMessage();
-    Map<Menu, Integer> orderMenu = input.inputOrderMenu();
-    order = new Order(date, orderMenu);
-  }
+    private void setOrder() {
+        inputView.viewMainMessage();
+        inputView.viewInputDateMessage();
+        int date = input.inputOrderDate();
+        inputView.viewInputMenuMessage();
+        Map<Menu, Integer> orderMenu = input.inputOrderMenu();
+        order = new Order(date, orderMenu);
+    }
 
 
 }
