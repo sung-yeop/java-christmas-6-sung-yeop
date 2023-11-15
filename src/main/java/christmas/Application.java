20aecd2cc7ac97f49d704d4fb2285c98bda0814c
sung-yeop <jjabc9459@naver.com>
2023-11-15 19:09:19 +0900

    feat(Application): 기능 실행 로직 추가
    - Controller를 통하여 작동할 수 있도록 로직 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.controller.Controller;
+import christmas.model.BadgeEvent;
+import christmas.model.Condition;
+import christmas.model.Discount;
+import christmas.model.Input;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller(new BadgeEvent(), new Condition(), new Discount()
+                , new Input(), new OutputView(), new InputView());
+        controller.start();
     }
 }
